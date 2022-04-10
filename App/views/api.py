d1f9c1dfa1618d5f3443f57ba593aa94869aa6f7
crowbar72 <tristanleid72@gmail.com>
2022-04-10 18:35:05 +0000

    commit on 'crowbar72-patch-1'

--- App/views/api.py
+++ App/views/api.py
@@ -3,5 +3,9 @@ from flask import Blueprint, redirect, render_template, request, send_from_direc
 api_views = Blueprint('api_views', __name__, template_folder='../templates')
 
 @api_views.route('/', methods=['GET'])
-def get_api_docs():
-    return render_template('index.html')
+def login():
+    return render_template('login.html')
+
+@api_views.route('/signup', methods=['GET'])
+def signup():
+    return render_template('signup.html')
