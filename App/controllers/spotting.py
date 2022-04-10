a53ad9631865b262e16544ae3df6b21e36d55e0b
satyaandm <satyaandm@gmail.com>
2022-04-10 16:04:23 -0400

    created spotting controller

---
+++ App/controllers/spotting.py
@@ -0,0 +1,36 @@
+from App.models import Spotting
+from App.database import db
+import json
+# from datetime import datetime
+
+def get_all_spottings():
+    return Spotting.query.all()
+
+
+def create_spotting(user_id, bird_name, lat, long, details):
+    newSpotting = Spotting(
+        user_id=user_id,
+        bird_name=bird_name,
+        lat=lat,
+        long=long,
+        details=details
+    )
+    newSpotting.set_time()
+    db.session.add(newSpotting)
+    db.session.commit()
+
+
+def get_all_spottings_json():
+    spottings = Spotting.query.all()
+    if not spottings:
+        return []
+    spottings = [spotting.toDict() for spotting in spottings]
+    return spottings
+
+
+def get_user_spottings(user_id):
+    spottings = Spotting.query.filter_by(user_id=user_id).all()
+    spottings = [spotting.toDict() for spotting in spottings]
+    return json.dumps(spottings)
+
+
