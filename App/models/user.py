5ae4a784d368e622b4ad8fdeb3f4abdc0cc1aedf
satyaandm <satyaandm@gmail.com>
2022-04-10 16:05:12 -0400

    updated user model to include spottings

--- App/models/user.py
+++ App/models/user.py
@@ -5,6 +5,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    spottings = db.relationship('Spotting', backref='user', lazy=True, cascade="all, delete-orphan")
 
     def __init__(self, username, password):
         self.username = username
@@ -13,7 +14,9 @@ class User(db.Model):
     def toDict(self):
         return{
             'id': self.id,
-            'username': self.username
+            'username': self.username,
+            'num_spottings': self.get_num_spottings(),
+            'spottings': self.spottings
         }
 
     def set_password(self, password):
@@ -23,4 +26,7 @@ class User(db.Model):
     def check_password(self, password):
         """Check hashed password."""
         return check_password_hash(self.password, password)
+
+    def get_num_spottings(self):
+        return len(self.spottings)
 
