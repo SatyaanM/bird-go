e948a9a88ecd6da7522661debe3a0abedb707d26
satyaandm <satyaandm@gmail.com>
2022-04-10 16:25:36 -0400

    added commands to test: create-spotting, get-spottings, get-user-spottings

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,13 @@ from flask.cli import with_appcontext
 
 from App.database import create_db
 from App.main import app, migrate
-from App.controllers import ( create_user, get_all_users_json )
+from App.controllers import (
+    create_user,
+    get_all_users_json,
+    create_spotting,
+    get_all_spottings_json,
+    get_user_spottings
+)
 
 
 @app.cli.command("init")
@@ -22,3 +28,22 @@ def create_user_command(username, password):
 @app.cli.command("get-users")
 def get_users():
     print(get_all_users_json())
+
+@app.cli.command("create-spotting")
+@click.argument("user_id")
+@click.argument("bird_name")
+@click.argument("lat")
+@click.argument("long")
+@click.argument("details")
+def create_spotting_command(user_id, bird_name, lat, long, details):
+    create_spotting(user_id, bird_name, lat, long, details)
+    print(f"{bird_name} at {lat},{long} spotted")
+
+@app.cli.command("get-spottings")
+def get_spottings():
+    print(get_all_spottings_json())
+
+@app.cli.command("get-user-spottings")
+@click.argument("user_id")
+def get_user_spottings_command(user_id):
+    print(get_user_spottings(user_id))
