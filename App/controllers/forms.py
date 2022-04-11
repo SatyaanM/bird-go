0fa0be8807d71c3554234f5e4df13bc33574a78a
crowbar72 <tristanleid72@gmail.com>
2022-04-11 02:22:49 +0000

    commit

--- App/controllers/forms.py
+++ App/controllers/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, BooleanField, TextAreaField, SubmitField
 from wtforms.validators import InputRequired, EqualTo, Email
 
 class SignUp(FlaskForm):
@@ -10,5 +10,5 @@ class SignUp(FlaskForm):
 
 class LogIn(FlaskForm):
     username = StringField('username', validators=[InputRequired()])
-    password = PasswordField('New Password', validators=[InputRequired()])
-    submit = SubmitField('Login', render_kw={'class': 'btn waves-effect waves-light white-text'})
+    password = PasswordField('password', validators=[InputRequired()])
+    submit = SubmitField('Login', render_kw={'class': 'btn white-text'})
--- App/views/api.py
+++ App/views/api.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory
-from App.forms import SignUp, LogIn
+from App.controllers.forms import SignUp, LogIn
 
 api_views = Blueprint('api_views', __name__, template_folder='../templates')
 
@@ -8,10 +8,37 @@ def login():
     form = LogIn()
     return render_template('login.html', form=form)
 
+@api_views.route('/', methods=['POST'])
+def loginAction():
+    form = LogIn()
+    if form.validate_on_submit(): # respond to form submission
+        data = request.form
+        user = User.query.filter_by(username = data['username']).first()
+        if user and user.check_password(data['password']): # check credentials
+            flash('Logged in successfully.') # send message to next page
+            login_user(user) # login the user
+            return redirect(url_for('todos')) # redirect to main page if login successful
+    flash('Invalid credentials')
+    return redirect(url_for('account'))
+
 @api_views.route('/signup', methods=['GET'])
 def signup():
     form = SignUp()
     return render_template('signup.html', form=form)
+
+@api_views.route('/signup', methods=['POST'])
+def signupAction():
+    form = SignUp() # create form object
+    if form.validate_on_submit():
+        data = request.form # get data from form submission
+        newuser = User(username=data['username']) # create user object
+        newuser.set_password(data['password']) # set password
+        db.session.add(newuser) # save new user
+        db.session.commit()
+        flash('Account Created!')# send message
+        return redirect(url_for('login'))# redirect to login page
+    flash('Error invalid input!')
+    return redirect(url_for('signup')) 
 
 @api_views.route('/account')
 def account():
