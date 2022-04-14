b715b4a888395ada9472f3da38c850241f0dcb72
satyaandm <satyaandm@gmail.com>
2022-04-14 12:26:06 -0400

    created wsgi commands for creating and getting spottings

--- wsgi.py
+++ wsgi.py
@@ -9,7 +9,8 @@ from App.controllers import (
     get_all_users_json,
     create_spotting,
     get_all_spottings_json,
-    get_user_spottings
+    get_spottings_by_user,
+    get_spottings_by_bird
 )
 
 
@@ -18,6 +19,7 @@ def initialize():
     create_db(app)
     print('database intialized')
 
+
 @app.cli.command("create-user")
 @click.argument("username")
 @click.argument("password")
@@ -25,9 +27,11 @@ def create_user_command(username, password):
     create_user(username, password)
     print(f'{username} created!')
 
+
 @app.cli.command("get-users")
 def get_users():
     print(get_all_users_json())
+
 
 @app.cli.command("create-spotting")
 @click.argument("user_id")
@@ -39,11 +43,19 @@ def create_spotting_command(user_id, bird_name, lat, long, details):
     create_spotting(user_id, bird_name, lat, long, details)
     print(f"{bird_name} at {lat},{long} spotted")
 
+
 @app.cli.command("get-spottings")
 def get_spottings():
     print(get_all_spottings_json())
 
+
 @app.cli.command("get-user-spottings")
 @click.argument("user_id")
 def get_user_spottings_command(user_id):
-    print(get_user_spottings(user_id))
+    print(get_spottings_by_user(user_id))
+
+
+@app.cli.command("get-bird-spottings")
+@click.argument("bird_name")
+def get_bird_spottings_command(bird_name):
+    print(get_spottings_by_bird(bird_name))
