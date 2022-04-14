f3bc3567792715de15c6c65f5dc94a13c5b4f93b
satyaandm <satyaandm@gmail.com>
2022-04-14 12:24:44 -0400

    spotting api calls

--- App/views/api.py
+++ App/views/api.py
@@ -1,3 +1,22 @@
 from flask import Blueprint
+import json
+
+from App.controllers import get_all_spottings_json, get_spottings_by_user, get_spottings_by_bird
 
 api_views = Blueprint("api_views", __name__, template_folder="../templates")
+
+
+@api_views.route('/spottings', methods=['GET'])
+def get_all_spottings():
+    return json.dumps(get_all_spottings_json())
+
+
+@api_views.route('/spottings/u/<user_id>', methods=['GET'])
+def get_user_spottings(user_id):
+    return json.dumps(get_spottings_by_user(user_id=user_id))
+
+
+@api_views.route('/spottings/b/<bird_name>', methods=['GET'])
+def get_bird_spottings(bird_name):
+    return json.dumps(get_spottings_by_bird(bird_name=bird_name))
+
