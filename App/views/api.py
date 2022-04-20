e18c93910aced71dff153414c77a1997baf8bb09
satyaandm <satyaandm@gmail.com>
2022-04-19 22:14:15 -0400

    added get bird list to api

--- App/views/api.py
+++ App/views/api.py
@@ -2,7 +2,7 @@ import json
 
 from flask import Blueprint, request, session
 
-from App.controllers import get_all_spottings_json, get_spottings_by_user, get_spottings_by_bird, set_user_location
+from App.controllers import set_user_location
 
 api_views = Blueprint("api_views", __name__, template_folder="../templates")
 
@@ -14,17 +14,9 @@ def set_user_location_route():
     return json.dumps({"location": f"({data['lat']}, {data['long']})"}), 200
 
 
-# @api_views.route('/spottings', methods=['GET'])
-# def get_all_spottings():
-#     return json.dumps(get_all_spottings_json())
-#
-#
-# @api_views.route('/spottings/u/<user_id>', methods=['GET'])
-# def get_user_spottings(user_id):
-#     return json.dumps(get_spottings_by_user(user_id=user_id))
-#
-#
-# @api_views.route('/spottings/b/<bird_name>', methods=['GET'])
-# def get_bird_spottings(bird_name):
-#     return json.dumps(get_spottings_by_bird(bird_name=bird_name))
-
+@api_views.route('/birds', methods=['GET'])
+def get_birds():
+    f = open('bird_species.json', 'r')
+    data = json.load(f)
+    f.close()
+    return data
