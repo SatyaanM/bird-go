d59e836e8cf4fa756cb9498ca3664b9bf8f5dd1d
satyaandm <satyaandm@gmail.com>
2022-04-20 15:28:30 -0400

    database url updated

--- App/config.py
+++ App/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "postgres://gleezppsiyjgnd:61ecb468cf4f1b392c165a6301f7b58a99b9296ab6077160431dcc0ec0cebadf@ec2-52-86-56-90.compute-1.amazonaws.com:5432/d8gtpod4afclfd"
+SQLALCHEMY_DATABASE_URI = "postgresql://gleezppsiyjgnd:61ecb468cf4f1b392c165a6301f7b58a99b9296ab6077160431dcc0ec0cebadf@ec2-52-86-56-90.compute-1.amazonaws.com:5432/d8gtpod4afclfd"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 7
 ENV = "DEVELOPMENT"
