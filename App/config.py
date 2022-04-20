6d31abf966957ac4f73ce658970b7d5c0161168f
satyaandm <satyaandm@gmail.com>
2022-04-20 03:19:47 -0400

    updated python version

--- App/config.py
+++ App/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "postgres://lkotxlrvslurhl:fd4cee9183dbcecdc8ad96dbc4224e11ee21bcd942b5746766991ec01bde6e6c@ec2-23-20-224-166.compute-1.amazonaws.com:5432/dc3mdj6dasvkh"
+SQLALCHEMY_DATABASE_URI = "postgresql://lkotxlrvslurhl:fd4cee9183dbcecdc8ad96dbc4224e11ee21bcd942b5746766991ec01bde6e6c@ec2-23-20-224-166.compute-1.amazonaws.com:5432/dc3mdj6dasvkh"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 7
 ENV = "DEVELOPMENT"
